17396f7b0522919427460be3ac8447d4096738c0
Kyle <dahaydnator@gmail.com>
2024-06-03 12:01:31 -0500

    connected server to client, added client folder

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+mutation Mutation($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;  
