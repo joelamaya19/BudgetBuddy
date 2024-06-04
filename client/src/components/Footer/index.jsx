bcc4cae66187d1ec0e562e59895e80271568e16a
NotSnowWhite <gflatch@att.net>
2024-06-04 14:04:21 -0500

    created header and nav elements for header and footer and list elements for footer.

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,5 +1,47 @@
-const Footer = () => {
+import { useLocation, useNavigate } from 'react-router-dom';
 
+const Footer = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    return (
+        <footer>
+            <div className="container">
+                {location.pathname !== '/' && (
+                    <button
+                        className="btn btn-dark mb-3"
+                        onClick={() => navigate(-1)}
+                    >
+                        &larr; Go Back
+                    </button>
+                )}
+                <h4>
+                    Made by the Three Musketeers.
+                </h4>
+                <div>
+                    <p className='contact'>Contact Us:</p>
+                    <ul className='social-links'>
+                        <li>
+                            <a className="gabi" href="https://github.com/NotSnowWhite">
+                                Gabi Latch
+                            </a>
+                        </li>
+                        <li>
+                            <a className="kyle" href="https://github.com/KyleH-Git">
+                                Kyle Hayden
+                            </a>
+                        </li>
+                        <li>
+                            <a className="joel" href="https://github.com/joelamaya19">
+                                Joel Amaya
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+        </footer>
+    )
 };
 
 export default Footer;
--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,5 +1,48 @@
-const Header = () => {
+// import link module and the functions from auth
+import { Link } from 'react-router-dom';
+import auth from '../../utils/auth';
 
+const Header = () => {
+// call logout() from auth, prevent default, and save in a variable called logout
+const logout = (event) => {
+    event.preventDefault();
+    auth.logout();
+};
+
+return (
+    <header className= "flex-row align-center">
+        <div className= "container">
+            <div>
+                <h1>BudgetBuddy</h1>
+                <p>A comprehensive expense tracker that helps you stick to your budget and track where your money is going....</p>
+            </div>
+            {/* nav element to hold buttons to navigate to other pages */}
+            <nav>
+                {/* if logged in, display profile link and logout button */}
+            {auth.loggedIn() ? (
+            <>
+              <Link className="btn profile-btn m-2" to="/">
+                {auth.getProfile().data.username}'s profile
+              </Link>
+              <button className="btn logout-btn m-2" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            // if not logged in, display login/signup buttons to navigate to the respective pages
+            <>
+              <Link className="btn login-btn m-2" to="/login">
+                Login
+              </Link>
+              <Link className="btn signup-btn m-2" to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
+            </nav>
+        </div>
+    </header>
+)
 };
 
 export default Header;
