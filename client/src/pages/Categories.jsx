53f8d514f5a7b7faf2ad78ec49a58d29b898e51a
Kyle <dahaydnator@gmail.com>
2024-06-05 11:49:54 -0500

    added categoryList component

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,7 +1,35 @@
 import CategoryForm from '../components/CategoriesForm/index';
+import { useQuery } from '@apollo/client';
+import { QUERY_CATEGORIES } from '../utils/queries';
+import Auth from '../utils/auth'
+import CategoryList from '../components/CategoriesList';
+
 const Categories = () => {
+    const token = Auth.getProfile();
+
+    const {loading, data } = useQuery(QUERY_CATEGORIES, {
+        variables: {name: token.data.name}
+    });
+
+    
+    const category = data?.categories || [];
+    console.log(data, 'data array');
+    console.log(category, 'category array');
+
     return (
-        <CategoryForm />
+        <main>
+            <CategoryForm />
+            <div className="col-12 col-md-8 mb-3">
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <CategoryList
+                            category={category}
+                            // more props?
+                        />
+                    )}
+                </div>
+        </main>
     );
 };
 
